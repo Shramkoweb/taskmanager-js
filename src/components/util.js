32e4874396095a4556a24576f7a0e85949364f4c
shramko.serhii <shramko.web@yahoo.com>
2019-08-14 21:53:38 +0300

    feat: add getRandomNumberInRange function to util

--- src/components/util.js
+++ src/components/util.js
@@ -13,4 +13,9 @@ const renderComponent = (container, component) => {
   return container.insertAdjacentHTML(`beforeend`, component);
 };
 
-export {getRandomBoolean, getRandomItemFrom, renderComponent};
+/* Получаем случайное число из диапазона */
+const getRandomNumberInRange = (min, max) => {
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+};
+
+export {getRandomBoolean, getRandomItemFrom, renderComponent, getRandomNumberInRange};
