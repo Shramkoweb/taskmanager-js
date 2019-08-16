7f9223ace728436bd4d5498b3c3c5d1d27cc883b
shramko.serhii <shramko.web@yahoo.com>
2019-08-16 08:44:30 +0300

    feat: implement dynamic load cards

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {renderComponent} from "./components/util";
 import {getCards, getFiltersCount} from "./data";
 
 const CARD_COUNT = 25;
-const MAX_CARD_TO_RENDER= 8;
+const MAX_CARD_TO_RENDER = 8;
 const CARDS = getCards(CARD_COUNT);
 
 
@@ -22,14 +22,23 @@ renderComponent(mainElement, getBoard(CARDS.slice(0, MAX_CARD_TO_RENDER)));
 const loadMoreButton = mainElement.querySelector(`.load-more`);
 const cardsBoardElement = mainElement.querySelector(`.board__tasks`);
 
-let cardsLeft = MAX_CARD_COUNT - cardsOnPage;
+let CARDS_ON_PAGE = MAX_CARD_TO_RENDER;
+let LEFT_CARDS_TO_RENDER = CARDS.length - CARDS_ON_PAGE;
 
 const renderLeftCards = () => {
-  renderComponent(cardsBoardElement, getBoardCards(CARDS.slice(cardsOnPage, MAX_CARD_COUNT)));
+  renderComponent(cardsBoardElement, getBoardCards(CARDS.slice(CARDS_ON_PAGE, (CARDS_ON_PAGE + MAX_CARD_TO_RENDER))));
+
+  CARDS_ON_PAGE = CARDS_ON_PAGE + MAX_CARD_TO_RENDER;
+  LEFT_CARDS_TO_RENDER = CARDS.length - CARDS_ON_PAGE;
+
+  if (LEFT_CARDS_TO_RENDER <= 0) {
+    loadMoreButton.classList.add(`visually-hidden`);
+    loadMoreButton.removeEventListener(`click`, onLoadMoreButtonClick);
+  }
 };
 
 const onLoadMoreButtonClick = () => {
-  renderLeftCards(cardsOnPage, MAX_CARD_COUNT);
+  renderLeftCards();
 };
 
 loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
