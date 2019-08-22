19246b6c228993810adf43d60befb5fc99951130
Serhii.Shramko <shramko.web@yahoo.com>
2019-08-22 21:49:31 +0300

    refactor: make Filter class

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,24 +1,28 @@
-const getFilter = ({title, count}) => {
-  return `
-      <input
-        type="radio"
-        id="filter__${title}"
-        class="filter__input visually-hidden"
-        name="filter"
-        ${count === 0 ? `disabled` : ``}
-        ${title === `all` ? `checked` : ``}
+import {createElement} from "../util";
+
+export class Filter {
+  constructor({title, count}) {
+    this.title = title;
+    this.count = count;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  getTemplate() {
+    return `
+      <input type="radio" id="filter__${this.title}" class="filter__input visually-hidden" name="filter"
+        ${this.count === 0 ? `disabled` : ``}
+        ${this.title === `all` ? `checked` : ``}
       />
-      <label for="filter__${title}" class="filter__label">
-        ${title} <span class="filter__${title}-count">${count}</span>
+      <label for="filter__${this.title}" class="filter__label">
+        ${this.title} <span class="filter__${this.title}-count">${this.count}</span>
       </label>
-  `.trim();
-};
-
-
-export const getFilters = (filters) => {
-  return `
-    <section class="main__filter filter container">
-      ${filters.map((filter) => getFilter(filter)).join(``)}
-    </section>
-  `.trim();
-};
+    `.trim();
+  }
+}
