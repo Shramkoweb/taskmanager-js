fc6ac53f6bb17f3a0d44c4cc39dfda01d3898554
Serhii.Shramko <shramko.web@yahoo.com>
2019-08-22 10:10:37 +0300

    feat: add util functions to util module

--- src/util.js
+++ src/util.js
@@ -8,14 +8,40 @@ const getRandomBoolean = () => {
   return Boolean(Math.round(Math.random()));
 };
 
-/* Ф-я рендера компонента */
-const renderComponent = (container, component) => {
-  return container.insertAdjacentHTML(`beforeend`, component);
-};
-
 /* Получаем случайное число из диапазона */
 const getRandomNumberInRange = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
 };
 
-export {getRandomBoolean, getRandomItemFrom, renderComponent, getRandomNumberInRange};
+const Position = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
+
+/* Ф-я рендера компонента */
+const renderElement = (container, element, place) => {
+  switch (place) {
+    case Position.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case Position.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+/* Убираем елемент если он есть */
+const unrenderElement = (element) => {
+  if (element) {
+    element.remove();
+  }
+};
+
+export {getRandomItemFrom, unrenderElement, renderElement, createElement, getRandomNumberInRange, getRandomBoolean};
+
