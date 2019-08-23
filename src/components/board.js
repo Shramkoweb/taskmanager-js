e4460463bca0b9807ab3114875ff78f397661b72
Serhii.Shramko <shramko.web@yahoo.com>
2019-08-23 20:46:39 +0300

    refactor: add Board class

--- src/components/board.js
+++ src/components/board.js
@@ -1,15 +1,38 @@
-import {getCard} from "./task";
-import {getCardEdit} from "./task-edit";
+import Task, {getCard} from "./task";
+import TaskEdit, {getCardEdit} from "./task-edit";
 import {getLoadMoreButton} from "./load-more-button";
+import {createElement} from "../util";
 
-/* Ф-я гереации карточек */
+/*/!* Ф-я гереации карточек *!/
 export const getBoardCards = (cards) => {
   return cards.map((card) => getCard(card)).join(``);
 };
 
-/* Ф-я генерации разметки борда для карточек */
+/!* Ф-я генерации разметки борда для карточек *!/
 export const getBoard = (cards) => {
   return `
+
+  `.trim();
+};*/
+
+export default class Board {
+  constructor(tasks) {
+    this._tasks = tasks;
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  getTemplate() {
+    const getEditTaskTemplate = new TaskEdit(this._tasks[0]).getTemplate();
+    const getTaskTemplate = this._tasks.map((task) => new Task(task).getTemplate()).join(``);
+    return `
       <section class="board container">
         <div class="board__filter-list">
           <a href="#" class="board__filter">SORT BY DEFAULT</a>
@@ -18,11 +41,12 @@ export const getBoard = (cards) => {
         </div>
         
         <div class="board__tasks">
-          ${getCardEdit(cards[0])}
-          ${getBoardCards(cards.slice(1))}
+          ${getEditTaskTemplate}
+          ${getTaskTemplate}
         </div>
         
         ${getLoadMoreButton()}
       </section>
-  `.trim();
-};
+    `.trim();
+  }
+}
