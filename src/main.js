119c8475d840db94ad40ac13960f8d3729a8d4c4
shramko.serhii <shramko.web@yahoo.com>
2019-08-27 21:16:26 +0300

    feat: add new realisation for empty board

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import Task from "./components/task";
 import TaskEdit from "./components/task-edit";
 import Board from "./components/board";
 import Search from "./components/search";
+import LoadMoreButton from "./components/load-more-button";
+import EmptyBoard from "./components/empty-board";
 
 
 const cardAmount = getRandomNumberInRange(8, 25); // Добавил для более навглядной проверки фильтров
@@ -34,81 +36,91 @@ const renderSearch = () => {
   renderElement(mainElement, search.getElement(), Position.BEFOREEND);
 };
 
-const board = new Board().getElement();
-const tasksContainer = board.querySelector(`.board__tasks`);
-
-const renderTasks = (tasks) => {
-  const fragment = document.createDocumentFragment();
-
-
-  tasks.forEach((task) => {
-    const taskInstance = new Task(task);
-    const taskEditInstance = new TaskEdit(task);
-
-    const closeEditTask = () => {
-      tasksContainer.replaceChild(taskInstance.getElement(), taskEditInstance.getElement());
-      document.removeEventListener(`keydown`, onTaskEditEscPress);
-    };
-
-    const openEditTask = () => {
-      tasksContainer.replaceChild(taskEditInstance.getElement(), taskInstance.getElement());
-      document.addEventListener(`keydown`, onTaskEditEscPress);
-    };
-
-    const onTaskEditEscPress = (evt) => isEscKeyDown(evt, closeEditTask);
-    const onEditTaskButtonClick = () => openEditTask();
-
-    taskInstance.getElement()
-      .querySelector(`.card__btn--edit`)
-      .addEventListener(`click`, onEditTaskButtonClick);
-
-    taskEditInstance.getElement().querySelector(`textarea`)
-      .addEventListener(`focus`, () => {
-        document.removeEventListener(`keydown`, onTaskEditEscPress);
-      });
-
-    taskEditInstance.getElement().querySelector(`textarea`)
-      .addEventListener(`blur`, () => {
-        document.addEventListener(`keydown`, onTaskEditEscPress);
-      });
-
-    taskEditInstance.getElement()
-      .querySelector(`.card__save`)
-      .addEventListener(`click`, closeEditTask);
-
-    fragment.appendChild(taskInstance.getElement());
-  });
-
-  return fragment;
-};
-
 const renderBoard = (tasks) => {
-  tasksContainer.appendChild(renderTasks(tasks));
+  const allTasksAmount = getFiltersCount(tasks)[0].count;
 
-  renderElement(mainElement, board, Position.BEFOREEND);
+  // Если активных карточек 0 то рисуем борду с сообщением
+  if (allTasksAmount === 0) {
+    const boardInstance = new EmptyBoard();
+    renderElement(mainElement, boardInstance.getElement(), Position.BEFOREEND);
+  } else { // Иначе рисуем карточки
+    const board = new Board().getElement();
+    const tasksContainer = board.querySelector(`.board__tasks`);
+    const loadMoreButtonInstance = new LoadMoreButton();
+
+    // Рендер карточек
+    const renderTasks = (taskMock) => {
+      const fragment = document.createDocumentFragment();
+
+      taskMock.forEach((task) => {
+        const taskInstance = new Task(task);
+        const taskEditInstance = new TaskEdit(task);
+
+        const closeEditTask = () => {
+          tasksContainer.replaceChild(taskInstance.getElement(), taskEditInstance.getElement());
+          document.removeEventListener(`keydown`, onTaskEditEscPress);
+        };
+
+        const openEditTask = () => {
+          tasksContainer.replaceChild(taskEditInstance.getElement(), taskInstance.getElement());
+          document.addEventListener(`keydown`, onTaskEditEscPress);
+        };
+
+        const onTaskEditEscPress = (evt) => isEscKeyDown(evt, closeEditTask);
+        const onEditTaskButtonClick = () => openEditTask();
+
+        taskInstance.getElement()
+          .querySelector(`.card__btn--edit`)
+          .addEventListener(`click`, onEditTaskButtonClick);
+
+        taskEditInstance.getElement().querySelector(`textarea`)
+          .addEventListener(`focus`, () => {
+            document.removeEventListener(`keydown`, onTaskEditEscPress);
+          });
+
+        taskEditInstance.getElement().querySelector(`textarea`)
+          .addEventListener(`blur`, () => {
+            document.addEventListener(`keydown`, onTaskEditEscPress);
+          });
+
+        taskEditInstance.getElement()
+          .querySelector(`.card__save`)
+          .addEventListener(`click`, closeEditTask);
+
+        fragment.appendChild(taskInstance.getElement());
+      });
+
+      return fragment;
+    };
+
+    tasksContainer.appendChild(renderTasks(tasks.slice(0, MAX_CARD_TO_RENDER)));
+    board.appendChild(loadMoreButtonInstance.getElement());
+    renderElement(mainElement, board, Position.BEFOREEND);
+
+    const loadMoreButton = mainElement.querySelector(`.load-more`);
+    const cardsBoardElement = mainElement.querySelector(`.board__tasks`);
+
+    const renderLeftCards = () => {
+      cardsBoardElement.appendChild(renderTasks(cards.slice(CARDS_ON_PAGE, (CARDS_ON_PAGE + MAX_CARD_TO_RENDER))));
+
+      CARDS_ON_PAGE = CARDS_ON_PAGE + MAX_CARD_TO_RENDER;
+      LEFT_CARDS_TO_RENDER = cards.length - CARDS_ON_PAGE;
+
+      if (LEFT_CARDS_TO_RENDER <= 0) {
+        unrenderElement(loadMoreButton);
+      }
+    };
+
+    const onLoadMoreButtonClick = () => {
+      renderLeftCards();
+    };
+
+    loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
+  }
+
 };
 
 renderMenu();
 renderSearch();
 renderFilters();
-renderBoard(cards.slice(0, MAX_CARD_TO_RENDER));
-
-const loadMoreButton = mainElement.querySelector(`.load-more`);
-const cardsBoardElement = mainElement.querySelector(`.board__tasks`);
-
-const renderLeftCards = () => {
-  cardsBoardElement.appendChild(renderTasks(cards.slice(CARDS_ON_PAGE, (CARDS_ON_PAGE + MAX_CARD_TO_RENDER))));
-
-  CARDS_ON_PAGE = CARDS_ON_PAGE + MAX_CARD_TO_RENDER;
-  LEFT_CARDS_TO_RENDER = cards.length - CARDS_ON_PAGE;
-
-  if (LEFT_CARDS_TO_RENDER <= 0) {
-    unrenderElement(loadMoreButton);
-  }
-};
-
-const onLoadMoreButtonClick = () => {
-  renderLeftCards();
-};
-
-loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
+renderBoard(cards);
